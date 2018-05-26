954f039eb33a1de8fae3a74ac153fc8dd3eea5e7
linux <linux@LINUXBBFD>
2018-05-26 11:06:37 +0200

    Add project files.

---
+++ TransportBrunaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TransportBrunaWeb.Startup))]
+namespace TransportBrunaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
