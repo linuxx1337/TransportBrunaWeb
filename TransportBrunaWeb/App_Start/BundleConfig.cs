236c5f364291ed56c7dd7cd66de2387236df5663
linux <linux@LINUXBBFD>
2018-08-29 12:29:01 +0200

    css test

--- TransportBrunaWeb/App_Start/BundleConfig.cs
+++ TransportBrunaWeb/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TransportBrunaWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
